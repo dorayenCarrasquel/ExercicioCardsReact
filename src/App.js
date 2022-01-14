76463af9684fe70f2c0d7cdd074a45a7026a35e0
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2022-01-14 09:09:01 -0300

    feat: clean de index using a app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import react from "react";
+import Card from "./components/Card";
+import './App.css'
+
+export default () =>
+<div>
+    <Card title = "Dora Marquez" conteudo = "Dora Márquez is the main character and hostess of the Dora the Explorer and Dora and Friends: Into the City! television series. She is a heroic Latina girl who embarks on countless adventures in every episode in order to find something or help somebody in need." />
+    <Card title = "Boots" conteudo = "Boots is a five-and-a-half (four for the first 12 episodes, and later five) year old furry monkey and Dora's best friend who is the co-host of the series. He likes to hold Dora's hand. Unlike Dora, Boots sometimes makes wrong choices and gets discouraged. When he is feeling down, he always bounces back quickly with one of his flips. No one can make Dora laugh the way Boots can."/>
+    <Card title = "Backpack" conteudo = "Backpack (or Mochila in Spanish) is a friend and special helper to Dora who helps her on all of Dora's adventures. Just like Dora, Backpack can speak both Spanish and English. Backpack is a real go-getter with a can-do spirit. As Backpack says in her signature song, Anything that you might need, I've got inside for you." />
+    <Card title = "Map" conteudo = "Map (sometimes referred after The) is a main character on Dora the Explorer. He is Dora's helpful guide who lives inside Backpack's side pocket."/>
+    <Card title = "Swiper" conteudo = "Swiper is the antagonist (and sometimes the anti-hero) in the television series and the secondary antagonist of Dora and the Lost City of Gold and appears in almost every episode. He is a 10 (9 for the first 50 episodes) year old sneaky orange fox who loves to swipe anything. As his name implies, Swiper steals or attempts to steal key items that help Dora on her adventures. "/>
+  </div>
